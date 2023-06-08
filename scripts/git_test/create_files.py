93b7e8c960c5f174226712db6471b1f3d7439de1
pigmeister <nipuntopno77@gmail.com>
2023-06-08 18:08:53 +0530

    updated workflow

--- scripts/git_test/create_files.py
+++ scripts/git_test/create_files.py
@@ -10,12 +10,12 @@ query2 = '\n'.join(query2lines)
 
 file1dir = path + 'file1'
 os.mkdir(file1dir)
-file1path = path + 'file1/' + 'file1.sql'
+file1path = path + 'file1/' + 'file3.sql'
 with open(file1path, 'w+') as file1:
     file1.write(query1)
 
 file2dir = path + 'file2'
 os.mkdir(file2dir)
-file2path = path + 'file2/' + 'file2.sql'
+file2path = path + 'file2/' + 'file4.sql'
 with open(file2path, 'w+') as file2:
     file2.write(query2)
