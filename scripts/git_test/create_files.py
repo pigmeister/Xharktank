a173e8ab9e163b932a161aaca95c620698dc4c25
pigmeister <nipuntopno77@gmail.com>
2023-07-14 14:45:45 +0530

    updated script

--- scripts/git_test/create_files.py
+++ scripts/git_test/create_files.py
@@ -1,16 +1,16 @@
 import os
 import git
 from git import Repo
+import json
+import requests
 
 repo_path = os.getcwd() + '/.scripts/node-weather-website'
 
 Repo.clone_from(url='git@github.com:pigmeister/node-weather-website.git', to_path=repo_path, branch='test-base')
 
-repo = Repo(path='./.scripts/node-weather-website')
+repo = Repo(path='/.scripts/node-weather-website')
 
-new_branch = repo.create_head('test-head')
-
-new_branch.checkout()
+repo.git.checkout('test-head')
 
 
 query1lines = ['hello', 'me', 'nipun']
@@ -19,14 +19,14 @@ query1 = '\n'.join(query1lines)
 query2lines = ['sup', 'is', 'me']
 query2 = '\n'.join(query2lines)
 
-file1dir = './.scripts/node-weather-website/' + 'file1'
+file1dir = '/.scripts/node-weather-website/' + 'file1'
 if not os.path.isdir(file1dir):
     os.mkdir(file1dir)
 file1path = file1dir + '/file1.sql'
 with open(file1path, 'w+') as file1:
     file1.write(query1)
 
-file2dir = './.scripts/node-weather-website/' + 'file2'
+file2dir = '/.scripts/node-weather-website/' + 'file2'
 if not os.path.isdir(file2dir):
     os.mkdir(file2dir)
 file2path = file2dir + '/file2.sql'
@@ -34,11 +34,26 @@ with open(file2path, 'w+') as file2:
     file2.write(query2)
 
 
-repo.config_writer().set_value("user", "name", "postmanbuilder[bot]").release()
-repo.config_writer().set_value("user", "email", "postmanbuilder[bot]@users.noreply.github.com").release()
+repo.config_writer().set_value("user", "name", "pigmeister-app[bot]").release()
+repo.config_writer().set_value("user", "email", "pigmeister-app[bot]@users.noreply.github.com").release()
 
 repo.git.add(repo_path)
 
 repo.git.commit('-m', 'Test commit')
 
 repo.git.push('origin', 'test-head')
+
+response = requests.post(
+    url=f'https://api.github.com/repos/pigmeister/node-weather-website/pulls',
+    headers={
+        'Authorization': 'Bearer %s' % os.environ.get('BRANCH'),
+        'Content-Type': 'application/json'
+    },
+    data=json.dumps({
+        'title': os.environ.get('BRANCH'),
+        'head': 'test-head',
+        'base': 'test-base'
+    })
+)
+
+print(response.json())
